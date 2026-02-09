231d38e3d97683e6b90104e7ddd1ce475df8c34d
Bilith10 <bilithvarghese10@gmail.com>
2026-02-09 23:38:21 +0100

    Message updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "Hello DevOps"}
+    return {"message": "Hello DevOps demo project work"}
 
 @app.route("/health")
 def health():
